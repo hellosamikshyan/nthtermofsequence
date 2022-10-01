3c977be4ea2aa78cdacb20b89ddd2bd90897e8b8
Samikshyan Mishra <87373078+hellosamikshyan@users.noreply.github.com>
2022-10-01 12:04:16 +0530

    Update tub13.py

--- tub13.py
+++ tub13.py
@@ -1,19 +1,3 @@
-from pyautogui import sleep
-
-
-names=["lore2","jfej","jif","jkjjf"]
-for i in names:
-    synonym="sir"
-    synonym=i+synonym
-    print(synonym)
-
-
-price=[23,55,22,55,5666,666]
-total=0
-for i in price:
-    total=i+total
-    print(total)
-    
 
 #Take the input from user and find the given term for sequences
 
